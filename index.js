c71ca937deb763ccb66ff9b5b1e4cdf889e3b412
Hedi Sabah <headesabah@gmail.com>
2018-09-11 06:20:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-$(document).ready(function (){
-});
+$.get( "https://api.github.com/users/AhmadAbdulkhaliq", function( data ) {
+console.log("success"+ data)
+})
+.fail(function() {
+    console.log('Something went wrong: ');
+});  
